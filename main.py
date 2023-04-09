5cd0b4870d56efc61bfad411ecd270bd40efefa3
Melvin Paulsam <49990182+MelvinPaulsam@users.noreply.github.com>
2023-04-09 15:01:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -70,3 +70,30 @@ if __name__ == "__main__":
           val_loader, args.log_dir, device, args.epochs,
           args.log_interval,
           args.load_weight_path, args.save_graph)
+    
+   CNN = Sequential(name="Sequential_CNN")
+
+CNN.add(Conv2D(16, kernel_size=(3, 3),
+               strides=(2, 2), padding="same",
+               activation="relu", input_shape=(28, 28, 1)))
+
+CNN.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2),
+                     padding="valid"))
+
+# Add another pair of Conv2D and MaxPooling2D for more model depth,
+# followed by the flatten and multiple dense layers
+
+CNN.add(Conv2D(32, kernel_size=(3, 3),
+               strides=(2, 2), padding="same",
+               activation="relu"))
+
+CNN.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2),
+                     padding="valid"))
+
+CNN.add(Flatten())
+
+CNN.add(Dense(64, activation='relu'))
+CNN.add(Dense(32, activation='relu'))
+CNN.add(Dense(10, activation='softmax'))
+
+CNN.summary()
